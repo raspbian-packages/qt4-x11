7f6926bee254238fe11cf61235fb3d2ad476867a
Debian Qt/KDE Maintainers <debian-qt-kde@lists.debian.org>
2015-06-14 16:38:43 +0000

    qt-everywhere-opensource-src-4.8.5-QTBUG-22829
    Gbp-Pq: Name qt-everywhere-opensource-src-4.8.5-QTBUG-22829.patch

--- src/tools/moc/main.cpp
+++ src/tools/moc/main.cpp
@@ -188,8 +188,9 @@ int runMoc(int _argc, char **_argv)
     pp.macros["Q_MOC_RUN"];
     pp.macros["__cplusplus"];
 
-    // Workaround a bug while parsing the boost/type_traits/has_operator.hpp header. See QTBUG-22829
+    // Workaround a bugs while parsing some boost headers. See QTBUG-22829 
     pp.macros["BOOST_TT_HAS_OPERATOR_HPP_INCLUDED"];
+    pp.macros["BOOST_LEXICAL_CAST_INCLUDED"];
 
     QByteArray filename;
     QByteArray output;
